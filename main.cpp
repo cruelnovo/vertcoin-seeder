b6d36fa5a5f6e8c959d6fba591d20fa38b77c7e4
Matthew Cummings <matt@gocummings.com>
2022-04-06 14:27:01 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -419,7 +419,7 @@ extern "C" void* ThreadStats(void*) {
 }
 
 static const string mainnet_seeds[] = {"useast1.vtconline.org", "vtc.gertjaap.org", ""};
-static const string testnet_seeds[] = {""};
+static const string testnet_seeds[] = {"vtc.ddnsking.com"};
 static const string *seeds = mainnet_seeds;
 
 extern "C" void* ThreadSeeder(void*) {
